51faa506abb6b4991cd3dd21f4a12732949289d2
artem <artemnzr@gmail.com>
2023-09-06 10:27:51 +0300

    setup

--- PP1/main.c
+++ PP1/main.c
@@ -2,19 +2,26 @@
 
 int main()
 {
-	char buf[15] = "Hello, world";
-	printf_s(buf);
+	char hello_buf[15] = "Hello, world\n";
+	char text_buf[50];
+	printf_s(hello_buf);
 	int choice;
 
-	/*
-	scanf_s(choice);
-	switch (choice)
+	while (1)
 	{
-	case 1:
-		
-	default:
-		printf_s(buf);
+		printf_s("Choose the command: ");
+		scanf_s("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			printf_s("test\n");
+			break;
+		case 2:
+
+		default:
+			printf_s(hello_buf);
+			break;
+		}
 	}
-	*/
 	return 0;
 }
