9d34d852a08de4a9f50234bb953226827a8c4412
artem <artem@DESKTOP-8U112FP>
2023-09-06 09:44:56 +0300

    Добавьте файлы проекта.

---
+++ PP1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	char buf[15] = "Hello, world";
+	printf_s(buf);
+	int choice;
+
+	/*
+	scanf_s(choice);
+	switch (choice)
+	{
+	case 1:
+		
+	default:
+		printf_s(buf);
+	}
+	*/
+	return 0;
+}
