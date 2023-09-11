06827a300d0d63516a858e79b88d4b4c9cf76b2c
AvangardAA <artemnzr@gmail.com>
2023-09-11 11:12:24 +0300

    case 2 case 3

--- PP1/main.c
+++ PP1/main.c
@@ -1,11 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
 	char hello_buf[15] = "Hello, world\n";
-	char text_buf[50];
+	char* text_buf = (char*)malloc(20 * sizeof(char));
 	printf_s(hello_buf);
 	int choice;
+
+	strcpy(text_buf, "Hello,World");
 
 	while (1)
 	{
@@ -17,11 +22,24 @@ int main()
 			printf_s("test\n");
 			break;
 		case 2:
-
+			text_buf[strlen(text_buf)] = "\n";
+			text_buf[strlen(text_buf) + 1] = "\0";
+			break;
+		case 3:
+			char namebuf[15];
+			scanf("%s", namebuf);
+			FILE *f = fopen(namebuf, "w");
+			fprintf(f, text_buf);
+			fclose(f);
+			break;
+		case 4:
+			printf_s("Jugg2");
+			break;
 		default:
-			printf_s(hello_buf);
+			printf_s(text_buf);
 			break;
 		}
 	}
+	free(text_buf);
 	return 0;
 }
