862a2b9d2dbce1e4eeceb871d42851ac2aa26617
Artem Nazarenko <70914823+AvangardAA@users.noreply.github.com>
2023-10-18 00:26:36 +0300

    PP3 done

---
+++ PP3/caesar.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <cstring>
+
+extern "C" 
+{
+    char* encrypt(char* input, int key) 
+    {
+        int len = strlen(input);
+        char* encr = new char[len + 1];
+
+        for (int i = 0; i < len; i++) 
+        {
+            char c = input[i];
+            if (isalpha(c)) 
+            {
+                char base = (isupper(c)) ? 'A' : 'a';
+                encr[i] = static_cast<char>(base + (c - base + key) % 26);
+            }
+            else 
+            {
+                encr[i] = c;
+            }
+        }
+        encr[len] = '\0';
+
+        return encr;
+    }
+
+    char* decrypt(char* encr, int key)
+    {
+        return encrypt(encr, 26 - key);
+    }
+
+}
---
+++ PP3/test.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstring>
+#include <dlfcn.h>
+
+char* encryptedMsg = nullptr;
+
+void cleanup() 
+{
+    if (encryptedMsg) 
+    {
+        delete[] encryptedMsg;
+    }
+}
+
+int main() 
+{
+    void* handle = dlopen("./caesar.dll", RTLD_LAZY);
+    if (!handle) 
+    {
+        std::cerr << "error loading" << dlerror() << std::endl;
+        return 1;
+    }
+
+    char* (*encrypt)(char*, int) = (char* (*)(char*, int))dlsym(handle, "encrypt");
+    char* (*decrypt)(char*, int) = (char* (*)(char*, int))dlsym(handle, "decrypt");
+
+    if (!encrypt || !decrypt) 
+    {
+        std::cerr << "error func load " << dlerror() << std::endl;
+        dlclose(handle);
+        return 1;
+    }
+
+    int choice;
+    char input[100];
+    int key;
+
+    while (true) 
+    {
+        std::cout << "Choose an option (1 enc, 2 decr, 3 exit): " << std::endl;
+        std::cin >> choice;
+        std::cin.ignore();
+
+        switch (choice) 
+        {
+        case 1:
+            std::cout << "Enter message to encrypt: ";
+            std::cin.getline(input, sizeof(input));
+            std::cout << "Enter key: ";
+            std::cin >> key;
+            std::cin.ignore();
+
+            if (encryptedMsg) 
+            {
+                delete[] encryptedMsg;
+            }
+            encryptedMsg = encrypt(input, key);
+            break;
+
+        case 2:
+            if (encryptedMsg) 
+            {
+                std::cout << "Enter decryption key: ";
+                std::cin >> key;
+                std::cin.ignore();
+                char* decr = decrypt(encryptedMsg, key);
+                std::cout << "Decrypted message: " << decr << std::endl;
+                delete[] decr;
+            }
+            else 
+            {
+                std::cout << "empty encrypted message" << std::endl;
+            }
+            break;
+
+        case 3:
+            cleanup();
+            dlclose(handle);
+            return 0;
+
+        default:
+            std::cout << "invalid option" << std::endl;
+        }
+    }
+}
