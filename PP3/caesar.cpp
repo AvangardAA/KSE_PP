de22fe172e2cffe4f1d1358167e1d7ad1bbd04fd
Artem Nazarenko <70914823+AvangardAA@users.noreply.github.com>
2023-10-18 09:39:20 +0300

    fix long key

--- PP3/caesar.cpp
+++ PP3/caesar.cpp
@@ -1,22 +1,23 @@
-#include <string>
+﻿#include <string>
 #include <cstring>
+#include <iostream>
 
-extern "C" 
+extern "C"
 {
-    char* encrypt(char* input, int key) 
+    char* encrypt(char* input, int key)
     {
         int len = strlen(input);
         char* encr = new char[len + 1];
 
-        for (int i = 0; i < len; i++) 
+        for (int i = 0; i < len; i++)
         {
             char c = input[i];
-            if (isalpha(c)) 
+            if (isalpha(c))
             {
                 char base = (isupper(c)) ? 'A' : 'a';
-                encr[i] = static_cast<char>(base + (c - base + key) % 26);
+                encr[i] = static_cast<char>(base + (c - base + ((key%26) + 26)) % 26);
             }
-            else 
+            else
             {
                 encr[i] = c;
             }
